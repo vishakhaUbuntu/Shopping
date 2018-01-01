88b8ccf2e4c4d8031d2dba2d6dd810004a41756a
vishakha <guptavishakha84@gmail.com>
2018-01-01 14:33:57 +0530

    Commit 2

--- amazon.php
+++ amazon.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Some New Page</title>
+        <title>Some New Page Rechanged</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
