d16660955890536755ea72d49a91ffed057233db
vishakhaUbuntu <31547647+vishakhaUbuntu@users.noreply.github.com>
2018-01-02 21:45:50 +0530

    Update addToDatabase.php

--- sql/addToDatabase.php
+++ sql/addToDatabase.php
@@ -1,41 +1,36 @@
 <?php
-include_once 'connection.php';
- 
+//include_once 'connection.php';
+require __DIR__ . '/../validations/phpValidations.php';
 /*------------------Get the value of all the variables of a form--------------------*/
 $first_name = filter_input(INPUT_POST, 'fname');
 $last_name = filter_input(INPUT_POST, 'lname');
 $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'passwd');
 
-/*------------------Encrypt the password and generate a salt string--------------------*/
-if($password != '')
+/*------------------Check for the validity of all the variables---------------------*/
+if(checkForStrings($first_name) && checkForStrings($last_name) && checkForEmails($email) && !empty($password))
 {
-  $md5Password = md5($password);                        //Encrpt the password and generate a string
-  $salt = bin2hex(openssl_random_pseudo_bytes(12));     //Generate a salt string
-  $combine = $salt.$md5Password;                        //Append password string to salt
-  $finalMD5 = md5($combine);                            //Encrypt salt and password together
-}
-else{
-  return false;
-}
+    $full_name = $first_name.' '.$last_name;                //Combine first and last name
+    
+    /*------------------Encrypt the password and generate a salt string--------------------*/
+      $md5Password = md5($password);                        //Encrpt the password and generate a string
+      $salt = bin2hex(openssl_random_pseudo_bytes(12));     //Generate a salt string
+      $combine = $salt.$md5Password;                        //Append password string to salt
+      $finalMD5 = md5($combine);                            //Encrypt salt and password together
+      
+    /*------------------Escape the string variables before putting in dbc-------------------*/
+      $full_name = $con->real_escape_string($full_name);
+      $email = $con->real_escape_string($email);
 
-//$newstr = filter_var($email, FILTER_SANITIZE_STRING);
-//$val = mysqli_real_escape_string($con, $email);
-
-$full_name = $first_name . ' ' . $last_name;
-//$newstr = filter_var($str, FILTER_SANITIZE_STRING);
-if(filter_var($email, FILTER_VALIDATE_EMAIL))
-    {
-      $query = $con->query("INSERT INTO first_table (full_name,email,password,salt) 
+    /*------------------Add the data to the db after all the validations--------------------*/  
+      $query = $con->query("INSERT INTO first_table (name,email,password,salt) 
                 VALUES ('$full_name','$email','$finalMD5','$salt')");
       if($query){
-        echo "Added";
-        return true;
+        return "Registered Successfully!";
       }
       else {
-        echo "Not added";
-        echo $con->error;
-        return false;
+        echo "Errormessage: ". $con->error;
+        return "Unable to register";
       }
-    }
+}
 ?>
